f86d3add7d13ad48c86277448bf3b5813ed87191
Daoab <38359000+Daoab@users.noreply.github.com>
2020-04-21 20:31:31 +0200

    Añadido movimiento suave, teletransporte y posibilidad de agarrar objetos *DAB*

---
+++ PracticaVR/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    [SerializeField] Transform teleportDst;
+    [SerializeField] float maxDistance = 5f;
+    [SerializeField] Transform rightHand;
+
+    RaycastHit hit;
+    int layerMask;
+
+    void Start()
+    {
+        teleportDst.gameObject.SetActive(false);
+        layerMask = LayerMask.GetMask("Floor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float button = Input.GetAxisRaw("Oculus_CrossPlatform_SecondaryIndexTrigger");
+
+        if(button > 0f)
+        {
+            if(Physics.Raycast(rightHand.position, rightHand.forward, out hit, maxDistance,  layerMask))
+            {
+                teleportDst.gameObject.SetActive(true);
+                teleportDst.transform.position = hit.point;
+            }
+            else
+            {
+                teleportDst.gameObject.SetActive(false);
+            }
+        }
+        else if (teleportDst.gameObject.activeSelf)
+        {
+            teleportDst.gameObject.SetActive(false);
+            transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        }
+    }
+}
