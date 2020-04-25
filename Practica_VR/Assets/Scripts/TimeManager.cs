072b92656c15bea4707e9a040b54481b56195071
Daoab <38359000+Daoab@users.noreply.github.com>
2020-04-25 18:53:44 +0200

    Añadido sistema de manejo de eventos en el tiempo

---
+++ Practica_VR/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimeManager : MonoBehaviour
+{
+    [System.Serializable]
+    class TimedEvent
+    {
+        public float time = 0f;
+        public UnityEvent events;
+    }
+
+    [SerializeField] TimedEvent[] timedEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("CallTimedEvents");
+    }
+
+    IEnumerator CallTimedEvents()
+    {
+        for(int i = 0; i < timedEvents.Length; i++)
+        {
+            yield return new WaitForSeconds(timedEvents[i].time);
+            timedEvents[i].events.Invoke();
+        }
+    }
+}
